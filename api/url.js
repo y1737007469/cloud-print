337ca4eaf24f887a520d0d8218767f3584050567
weiliang <535027650@qq.com>
2021-03-25 20:53:13 +0800

    gitee -> github

---
+++ api/url.js
@@ -0,0 +1,15 @@
+// "use strict"
+export default {
+// 公网服务器
+  URL_SERVER: "http://39.107.119.52:5000/v1/wechat/",
+// 本地
+  // URL_SERVER: "http://localhost:5000/v1/wechat/",
+  // 用户登录
+  URL_LOGIN: "user/login",
+  // 用户打印历史
+  URL_FILE_HISTORY: "order/orderlist",
+  // 上传用户数据
+  URL_USER_UPLOAD: "user/updateinfo",
+  // 上传文件
+  URL_FILE_UPLOAD: "Upload"
+}
